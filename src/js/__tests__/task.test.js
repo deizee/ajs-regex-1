bb181f6de7d78b6c43e76e4ff8a16be01d7f3394
Deizee <deizee@mail.ru>
2021-03-03 07:21:24 +0500

    fixed bugs

--- src/js/__tests__/task.test.js
+++ src/js/__tests__/task.test.js
@@ -5,42 +5,57 @@ test('Метод должен вернуть true', () => {
   expect(valid.validateUsername()).toBeTruthy();
 });
 
-test('Метод должен вернуть false', () => {
+test('Метод должен вернуть false, если имя начинается с -', () => {
   const valid = new Validator('-Ekaterina');
   expect(valid.validateUsername()).toBeFalsy();
 });
 
-test('Метод должен вернуть false', () => {
+test('Метод должен вернуть false, если имя заканчивается на -', () => {
   const valid = new Validator('Ekaterina-');
   expect(valid.validateUsername()).toBeFalsy();
 });
 
-test('Метод должен вернуть false', () => {
+test('Метод должен вернуть false, если имя начинается с _', () => {
   const valid = new Validator('_Ekaterina');
   expect(valid.validateUsername()).toBeFalsy();
 });
 
-test('Метод должен вернуть false', () => {
+test('Метод должен вернуть false, если имя заканчивается на _', () => {
   const valid = new Validator('Ekaterina_');
   expect(valid.validateUsername()).toBeFalsy();
 });
 
-test('Метод должен вернуть false', () => {
+test('Метод должен вернуть false, если имя начинается с цифры', () => {
   const valid = new Validator('7Ekaterina');
   expect(valid.validateUsername()).toBeFalsy();
 });
 
-test('Метод должен вернуть false', () => {
+test('Метод должен вернуть false, если имя заканчивается цифрой', () => {
   const valid = new Validator('Ekaterina7');
   expect(valid.validateUsername()).toBeFalsy();
 });
 
-test('Метод должен вернуть false', () => {
-  const valid = new Validator('Ekaterina7');
+test('Метод должен вернуть false, если в имени есть 4 цифры подряд', () => {
+  const valid = new Validator('Ekate7777rina');
   expect(valid.validateUsername()).toBeFalsy();
 });
 
-test('Метод должен вернуть false', () => {
-  const valid = new Validator('Ekate777rina');
+test('Метод должен вернуть true, если "запрещенные" символы внутри имени', () => {
+  const valid = new Validator('Eka-te_ri81na');
+  expect(valid.validateUsername()).toBeTruthy();
+});
+
+test('Метод должен вернуть false, если имя начинается с #', () => {
+  const valid = new Validator('#Ekaterina');
+  expect(valid.validateUsername()).toBeFalsy();
+});
+
+test('Метод должен вернуть false, если имя начинается с @', () => {
+  const valid = new Validator('@Ekaterina');
+  expect(valid.validateUsername()).toBeFalsy();
+});
+
+test('Метод должен вернуть false, если имя состоит из ##', () => {
+  const valid = new Validator('##');
   expect(valid.validateUsername()).toBeFalsy();
 });
